628e1387a71008fc2382b2d01bfcbbd44dbcb347
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-30 23:48:37 +0530

    Update: vector, push_back, pop_back (147 lines)

--- vector.cpp
+++ vector.cpp
@@ -130,7 +130,8 @@ void unordert_learning()
 
     // check if an element exists
     int t = 5;
-    if(s.find(t) !=s.end()) cout << t << "Found"
+    if(s.find(t) !=s.end()) cout << t << "Found" << endl;
+    else cout << t << "Not Found" << endl;
 
 }
 

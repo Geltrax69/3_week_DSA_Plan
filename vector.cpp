b7c646e08cf29a557590a5c5d5cfdc33613e141c
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-30 23:47:43 +0530

    Update: vector, push_back, pop_back (147 lines)

--- vector.cpp
+++ vector.cpp
@@ -128,7 +128,10 @@ void unordert_learning()
     }
     cout << endl;
 
-    for(auto)
+    for(auto it = s.begin(); it != s.end(); it++){
+        cout << *it << " ";
+    }
+    cout << endl;
 
 }
 

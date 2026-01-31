b30f3dd6e6cbbad5e56518a35065ccf7acf49a22
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 11:34:15 +0530

    Update: vector, push_back, pop_back (305 lines)

--- vector.cpp
+++ vector.cpp
@@ -281,7 +281,7 @@ void multimap_elements()
      mp.erase(mp.begin());
   cout << "Elements after deleting the first element: " << endl;
   cout << "Key\tElement" << endl;
-  for (auto it = mp.begin(); it != mp.end(); it++) {
+  for (auto it = mm.begin(); it != mm   .end(); it++) {
     cout << it -> first << "\t" << it -> second << endl;
   }
 }

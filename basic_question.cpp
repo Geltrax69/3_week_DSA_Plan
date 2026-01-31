f2464603cc2e731de93b9db5281596657b618d70
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 11:44:09 +0530

    Update basic_question: code update (20 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -5,7 +5,7 @@ int countDigits(int n){
     int ctr = 0;
     if (n <10) return 1; // Edge case for 0
     while(n!=0){
-        n/=10;
+        n/=10;              //Time Complexity: O(log10N + 1)
         ctr++;
     }
     return ctr;

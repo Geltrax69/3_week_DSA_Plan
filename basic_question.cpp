53bba278409ead35b9db0140f88a6176f599ac7e
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 12:06:39 +0530

    Update basic_question: code update (47 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -32,7 +32,10 @@ int reverseNumberOptimal(int n){
     if(n==0) return 0; // Base Case
     return (n%10 * pow(10, countDigitsOptimal(n)-1)) + reverseNumberOptimal(n/10); //Time Complexity: O(log10N + 1), Space Complexity: O(log10N + 1) due to recursive stack
 }
-int re
+int recursiveReverse(int n, int rev = 0){
+    if(n==0) return rev; // Base Case
+    return recursiveReverse(n/10, rev*10 + n%10); //Time Complexity: O(log10N + 1), Space Complexity: O(log10N + 1) due to recursive stack
+}
 
 int main() {
     int number;

87b8f1753dee6ae827679661bd2b11dcf370ddee
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 13:28:43 +0530

    Update basic_question: code update (128 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -79,7 +79,7 @@ string reverse_String(string s, int start, int end)
     return reverse_String(s,start+1,end-1); //Time Complexity: O(n/2), Space Complexity: O(n/2) due to recursive stack
 }
 
-int fib()
+int fib(int n)
 
 int main() {
     // int number;

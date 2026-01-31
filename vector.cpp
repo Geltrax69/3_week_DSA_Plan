360c15e07604938a0776e0c6df731ff733248fef
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 11:19:10 +0530

    Update: vector, push_back, pop_back (226 lines)

--- vector.cpp
+++ vector.cpp
@@ -197,7 +197,7 @@ void map_elements()
 void print_queue(queue<int> q)
 {
     while(!q.empty()){
-        cout << q.front() << " ";
+        cout << q.() << " ";
         q.pop();
     }
     cout << endl;

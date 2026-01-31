99de9aad3c4a54c34f25c814c0804cd6bf24f195
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 12:35:43 +0530

    Update basic_question: code update (81 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -74,6 +74,7 @@ int main() {
     cout << "Enter two numbers to find GCD: ";
     cin >> a >> b;
     cout << "GCD of " << a << " and " << b << " is: " << gcdnormal(a, b) << endl;
+    cout << "GCD of " << a << " and " << b << " (Optimal) is: " << gcdoptimal(a, b) << endl;
 
     return 0;
 }

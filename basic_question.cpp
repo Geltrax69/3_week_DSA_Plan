75e155fa0a72f4c134b0861ac20806d37bfb70b7
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 13:15:24 +0530

    Update basic_question: code update (112 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -99,8 +99,8 @@ int main() {
     string name;
     cout << "Enter your name: ";
     cin >> name;
-    cout << "Your name is: ";
-    nameRecursion(name,0,n);
+    // cout << "Your name is: ";
+    // nameRecursion(name,0,n);
 
     cout << endl;
     cout << "Reversed string is: ";

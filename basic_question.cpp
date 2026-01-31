68b23cb8d30e741366e030d1657fcc7860fa3dd6
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 12:35:27 +0530

    Update basic_question: code update (81 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -54,7 +54,7 @@ int gcdoptimal(int a, int b){
     if(b==0) return a;         //Time Complexity: O(log(min(a,b))), Space Complexity: O(log(min(a,b))) due to recursive stack
     return gcdoptimal(b, a%b);
 }
-int re
+int 
 
 int main() {
     // int number;

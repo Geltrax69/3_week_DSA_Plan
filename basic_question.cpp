5ff707f9cffa5a4800cdb8d759407d15e9fca50f
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 13:23:03 +0530

    Update basic_question: code update (115 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -69,7 +69,7 @@ string reverse_String(string s, int pos, int l)
 	if(l==0) return "";
 	cout<<s[l]<<"";
 	pos++;
-	return s[pos] + reverse_String(s,l);
+	return s[pos] + reverse_String(s,l--);
 }
 
 

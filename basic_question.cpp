c9fad1b9eee8f0af273fbf719c0d0849d7c99097
Geltrax69 <10lalitsingh01@gmail.com>
2026-01-31 11:45:49 +0530

    Update basic_question: code update (25 lines)

--- basic_question.cpp
+++ basic_question.cpp
@@ -13,7 +13,7 @@ int countDigits(int n){
 
 int countDigitsOptimal(int n){
     if (n == 0) return 1; // Edge case for 0
-    return (int)log10(n)
+    return (int)log10(n)+1;
 }
 int main() {
     int number;
